4e8396de894dba05d14b2e9e031944b669034dd4
Sayyidusy syauqi Al Ghiffari <sijeding15@gmail.com>
2025-02-09 03:11:39 +0700

    add all file/level

--- settings/documents.ts
+++ settings/documents.ts
@@ -86,6 +86,134 @@ export const Documents: Paths[] = [
         title: "Level 1 - 2",
         href: "/level1-2",
       },
+      {
+        title: "Level 2 - 3",
+        href: "/level2-3",
+      },
+      {
+        title: "Level 3 - 4",
+        href: "/level3-4",
+      },
+      {
+        title: "Level 4 - 5",
+        href: "/level4-5",
+      },
+      {
+        title: "Level 5 - 6",
+        href: "/level5-6",
+      },
+      {
+        title: "Level 6 - 7",
+        href: "/level6-7",
+      },
+      {
+        title: "Level 7 - 8",
+        href: "/level7-8",
+      },
+      {
+        title: "Level 8 - 9",
+        href: "/level8-9",
+      },
+      {
+        title: "Level 9 - 10",
+        href: "/level9-10",
+      },
+      {
+        title: "Level 10 - 11",
+        href: "/level10-11",
+      },
+      {
+        title: "Level 11 - 12",
+        href: "/level11-12",
+      },
+      {
+        title: "Level 12 - 13",
+        href: "/level12-13",
+      },
+      {
+        title: "Level 13 - 14",
+        href: "/level13-14",
+      },
+      {
+        title: "Level 14 - 15",
+        href: "/level14-15",
+      },
+      {
+        title: "Level 15 - 16",
+        href: "/level15-16",
+      },
+      {
+        title: "Level 16 - 17",
+        href: "/level16-17",
+      },
+      {
+        title: "Level 17 - 18",
+        href: "/level17-18",
+      },
+      {
+        title: "Level 18 - 19",
+        href: "/level18-19",
+      },
+      {
+        title: "Level 19 - 20",
+        href: "/level19-20",
+      },
+      {
+        title: "Level 20 - 21",
+        href: "/level20-21",
+      },
+      {
+        title: "Level 21 - 22",
+        href: "/level21-22",
+      },
+      {
+        title: "Level 22 - 23",
+        href: "/level22-23",
+      },
+      {
+        title: "Level 23 - 24",
+        href: "/level23-24",
+      },
+      {
+        title: "Level 24 - 25",
+        href: "/level24-25",
+      },
+      {
+        title: "Level 25 - 26",
+        href: "/level25-26",
+      },
+      {
+        title: "Level 26 - 27",
+        href: "/level26-27",
+      },
+      {
+        title: "Level 27 - 28",
+        href: "/level27-28",
+      },
+      {
+        title: "Level 28 - 29",
+        href: "/level28-29",
+      },
+      {
+        title: "Level 29 - 30",
+        href: "/level29-30",
+      },
+      {
+        title: "Level 30 - 31",
+        href: "/level30-31",
+      },
+      {
+        title: "Level 31 - 32",
+        href: "/level31-32",
+      },
+      {
+        title: "Level 32 - 33",
+        href: "/level32-33",
+      },
+      {
+        title: "Level 33 - 34",
+        href: "/level33-34",
+      },
     ],
   },
   {
